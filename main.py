1381b06c50ede85e738fc5405dbf364c18a2e44e
TyMaH <112333971+K2-TyMaH@users.noreply.github.com>
2022-10-30 16:56:31 +0200

    Add files via upload
    Усі зауваження прочитав та переробив.

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ def make_dir_func(path):
     INNER_FOLDERS_LIST = ('images', 'documents', 'audio', 'video', 'archives')
     new_folder = 'Sorted_folder'
     for item in INNER_FOLDERS_LIST:
-        Path(f'{path}\\{new_folder}\\{item}').mkdir(exist_ok=True, parents=True)
+        Path(path, new_folder, item).mkdir(exist_ok=True, parents=True)
+        #Path(f'{path}\\{new_folder}\\{item}').mkdir(exist_ok=True, parents=True)
+
 
 
 def file_scaner(folder_path: Path) -> None:
@@ -50,53 +52,66 @@ def file_scaner(folder_path: Path) -> None:
                 IMAGES_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 KNOWN_EXTENSIONS.add(f'{x.suffix}')
                 shutil.move(Path(x.absolute()),
-                            Path(f'{FOLDER_PATH}\\Sorted_folder\\images\\{normalize(x.stem)}{x.suffix}'))
+                            Path(FOLDER_PATH, 'Sorted_folder', 'images', f'{normalize(x.stem)}{x.suffix}'))
+                            #Path(f'{FOLDER_PATH}\\Sorted_folder\\images\\{normalize(x.stem)}{x.suffix}'))
 
             elif x.suffix[1:] in EXTENSIONS_DICT['documents']:
                 DOCUMENTS_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 KNOWN_EXTENSIONS.add(f'{x.suffix}')
                 shutil.move(Path(x.absolute()),
-                            Path(f'{FOLDER_PATH}\\Sorted_folder\\documents\\{normalize(x.stem)}{x.suffix}'))
+                            Path(FOLDER_PATH, 'Sorted_folder', 'documents', f'{normalize(x.stem)}{x.suffix}'))
+                            #Path(f'{FOLDER_PATH}\\Sorted_folder\\documents\\{normalize(x.stem)}{x.suffix}'))
 
             elif x.suffix[1:] in EXTENSIONS_DICT['audio']:
                 AUDIO_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 KNOWN_EXTENSIONS.add(f'{x.suffix}')
                 shutil.move(Path(x.absolute()),
-                            Path(f'{FOLDER_PATH}\\Sorted_folder\\audio\\{normalize(x.stem)}{x.suffix}'))
+                            Path(FOLDER_PATH, 'Sorted_folder', 'audio', f'{normalize(x.stem)}{x.suffix}'))
+                            #Path(f'{FOLDER_PATH}\\Sorted_folder\\audio\\{normalize(x.stem)}{x.suffix}'))
 
             elif x.suffix[1:] in EXTENSIONS_DICT['video']:
                 VIDEO_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 KNOWN_EXTENSIONS.add(f'{x.suffix}')
                 shutil.move(Path(x.absolute()),
-                            Path(f'{FOLDER_PATH}\\Sorted_folder\\video\\{normalize(x.stem)}{x.suffix}'))
+                            Path(FOLDER_PATH, 'Sorted_folder', 'video', f'{normalize(x.stem)}{x.suffix}'))
+                            #Path(f'{FOLDER_PATH}\\Sorted_folder\\video\\{normalize(x.stem)}{x.suffix}'))
 
             elif x.suffix[1:] in EXTENSIONS_DICT['archives']:
                 ARCHIVES_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 KNOWN_EXTENSIONS.add(f'{x.suffix}')
                 try:
                     shutil.unpack_archive(Path(x.absolute()),
-                                          Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}'))
+                                          Path(FOLDER_PATH, 'Sorted_folder', 'archives',
+                                               f'{normalize(x.stem)}{x.suffix}'))
+                                          #Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}'))
                     shutil.move(Path(x.absolute()),
-                                Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}{x.suffix}'))
+                                Path(FOLDER_PATH, 'Sorted_folder', 'archives', f'{normalize(x.stem)}{x.suffix}'))
+                                #Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}{x.suffix}'))
                 except shutil.ReadError:
                     print(f'Something become WRONG with {x}!')
                     shutil.move(Path(x.absolute()),
-                                Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}{x.suffix}'))
+                                Path(FOLDER_PATH, 'Sorted_folder', 'archives', f'{normalize(x.stem)}{x.suffix}'))
+                                #Path(f'{FOLDER_PATH}\\Sorted_folder\\archives\\{normalize(x.stem)}{x.suffix}'))
             else:
                 OTHER_LIST.append(f'{normalize(x.stem)}{x.suffix}')
                 UNKNOWN_EXTENSIONS.add(f'{x.suffix}')
                 shutil.move(Path(x.absolute()),
-                            Path(f'{FOLDER_PATH}\\Sorted_folder\\{normalize(x.stem)}{x.suffix}'))
+                            Path(FOLDER_PATH, 'Sorted_folder', f'{normalize(x.stem)}{x.suffix}'))
+                            #Path(f'{FOLDER_PATH}\\Sorted_folder\\{normalize(x.stem)}{x.suffix}'))
         elif x.is_dir():
             if x.name not in INNER_FOLDERS_LIST:
                 FOLDER_LIST.append(x)
                 file_scaner(x)
 
 
+def main():
+    if len(sys.argv) == 2:
+        folder_path = sys.argv[1]
+    else:
+        print("You didn't write normal path to your folder. Try again.")
 
-if __name__ == '__main__':
-    folder_path = sys.argv[1]
     print(f'Start in folder {folder_path}')
+    global FOLDER_PATH
     FOLDER_PATH = folder_path
     make_dir_func(Path(folder_path))
     file_scaner(Path(folder_path))
@@ -110,4 +125,7 @@ if __name__ == '__main__':
 
     print(f'Types of files in folder: {KNOWN_EXTENSIONS}')
     print(f'Unknown files of types: {UNKNOWN_EXTENSIONS}')
+
+if __name__ == '__main__':
+    main()
 
